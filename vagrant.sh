4c64699d4e594712f70c80bee37212f316f1853e
Christian Joudrey <cmallette@gmail.com>
2014-01-08 03:27:07 +0000

    Add vagrant setup script

---
+++ vagrant.sh
@@ -0,0 +1,28 @@
+PREFIX=/home/vagrant
+BINDIR="$PREFIX/bin"
+
+mkdir $BINDIR
+
+# Install matcher
+TEMP_MATCHER_FOLDER=`mktemp -d`
+git clone git@github.com:burke/matcher.git $TEMP_MATCHER_FOLDER \
+  && cd $TEMP_MATCHER_FOLDER \
+  && make install PREFIX=$PREFIX \
+
+# Install hub
+HUBDIR="$BINDIR/hub"
+TEMP_HUB_FOLDER=`mktemp -d`
+git clone https://github.com/github/hub.git $TEMP_HUB_FOLDER \
+  && cd $TEMP_HUB_FOLDER \
+  && rake install prefix=$BINDIR \
+
+# Setup dotfiles
+DOTFILES_FOLDER="$PREFIX/dotfiles"
+[ -d $DOTFILES_FOLDER ] && rm -rf $DOTFILES_FOLDER
+[ -f "$PREFIX/.bashrc" ] && rm "$PREFIX/.bashrc"
+
+git clone git@github.com:cjoudrey/dotfiles.git $DOTFILES_FOLDER \
+  && cd $DOTFILES_FOLDER \
+  && git checkout vagrant \
+  && rake install \
+  && rm "$PREFIX/.bash_profile" \
