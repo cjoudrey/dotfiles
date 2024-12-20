4b3cb87471bff6ce86c2a22f0f0666386f2f9160
Christian Joudrey <cmallette@gmail.com>
2024-12-20 11:07:37 -0500

    Update lazyvim

--- config/nvim/lua/plugins/ruby-lsp.lua
+++ config/nvim/lua/plugins/ruby-lsp.lua
@@ -1,11 +1,12 @@
 return {
   {
     "neovim/nvim-lspconfig",
+    dependencies = { "hrsh7th/cmp-nvim-lsp" },
     opts = {
       servers = {
         ruby_lsp = {
-          mason = false,
           cmd = { vim.fn.expand("~/.rbenv/shims/ruby-lsp") },
+          capabilities = require("cmp_nvim_lsp").default_capabilities(),
         },
         rubocop = {
           enabled = false,
