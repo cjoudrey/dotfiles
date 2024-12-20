865a22ca018091684928dad65cbe63eaf8b01b0c
Christian Joudrey <cmallette@gmail.com>
2024-12-19 22:17:32 -0500

    Fresh start

--- bashrc
+++ bashrc
@@ -1,16 +1,3 @@
-export PATH="/usr/local/bin:$PATH"
-
-export GO111MODULE=on
-export GOPATH=$HOME/code/go
-export GOBIN=$GOPATH/bin
-export PATH=$PATH:$GOBIN
-export PATH="$PATH:$HOME/.config/yarn/global/node_modules/.bin"
-export PATH="$PATH:$HOME/dotfiles/bin"
-
-# Load rvm (if installed)
-[[ -s "$HOME/.rvm/scripts/rvm" ]] && . "$HOME/.rvm/scripts/rvm"
-
-# Load rbenv (if installed)
 [[ -s "$HOME/.rbenv" ]] && eval "$(rbenv init -)"
 
 # Set vim as default editor
@@ -18,10 +5,6 @@ export EDITOR="vim"
 
 # VI movement in Bash
 set -o vi
-
-if [ -f ~/.git-completion.sh ]; then
-    source ~/.git-completion.sh
-fi
 
 # Aliases
 alias ll='ls -Fls'
@@ -33,24 +16,7 @@ alias ga='git add'
 alias gp='git push'
 alias be='bundle exec'
 alias k='kubectl'
-
-# sha256sum
-alias sha256sum='shasum -a 256'
-
-# json pretty-print
-alias pp='python -mjson.tool'
-
-# fix colors in tmux
-alias tmux="TERM=screen-256color-bce tmux"
-
-alias grep="ack"
-
-# Hub
-if hub --version >/dev/null 2>&1; then eval "$(hub alias -s)"; fi
-
-# Prevent iTerm from closing when hitting Control-D
-export IGNOREEOF=25
-
-export TIL_STORAGE_PATH="$HOME/til"
+alias grep="rg"
+alias ack="rg"
 
 eval "$(starship init bash)"
---
+++ config/nvim/lua/plugins/ruby-lsp.lua
@@ -0,0 +1,67 @@
+return {
+  {
+    "neovim/nvim-lspconfig",
+    opts = {
+      servers = {
+        ruby_lsp = {
+          mason = false,
+          cmd = { vim.fn.expand("~/.rbenv/shims/ruby-lsp") },
+        },
+        rubocop = {
+          enabled = false,
+        },
+      },
+    },
+  },
+}
+
+--- local ruby_version = vim.fn.executable("ruby") == 1 and vim.fn.system('ruby -e "puts RUBY_VERSION"'):gsub("\n", "")
+---   or nil
+---
+--- ---@type LazySpec
+--- return {
+---   {
+---     "williamboman/mason.nvim",
+---     opts = function(_, opts)
+---       -- Hack to have different installations depending on ruby version
+---       -- Ideally mason would manage gem installations differently
+---       if ruby_version ~= nil then
+---         local path = require("mason-core.path")
+---         opts.install_root_dir = path.concat({ tostring(vim.fn.stdpath("data")), "mason", "ruby", ruby_version })
+---         vim.notify("setting mason dir to " .. opts.install_root_dir)
+---       else
+---         vim.notify("`ruby` is not executable. Using default Mason installation directory.", vim.log.levels.WARN)
+---       end
+---       return opts
+---     end,
+---   },
+---   ---  {
+---   ---    "williamboman/mason-lspconfig.nvim",
+---   ---    dependencies = { "williamboman/mason.nvim" },
+---   ---    opts = function(_, opts)
+---   ---      opts.ensure_installed = require("astrocore").list_insert_unique(opts.ensure_installed, {
+---   ---        "lua_ls",
+---   ---        "jsonls",
+---   ---        "yamlls",
+---   ---        "eslint",
+---   ---      })
+---   ---
+---   ---      if ruby_version and ruby_version >= "2.7" then
+---   ---        opts.ensure_installed = require("astrocore").list_insert_unique(opts.ensure_installed, {
+---   ---          "ruby_lsp",
+---   ---          "sorbet",
+---   ---        })
+---   ---      end
+---   ---      opts.inlay_hints = { enabled = true }
+---   ---    end,
+---   ---  },
+---   ---  {
+---   ---    "jay-babu/mason-null-ls.nvim",
+---   ---    opts = function(_, opts)
+---   ---      opts.ensure_installed = require("astrocore").list_insert_unique(opts.ensure_installed, {
+---   ---        -- "stylua",
+---   ---        "jq",
+---   ---      })
+---   ---    end,
+---   ---  },
+--- }
